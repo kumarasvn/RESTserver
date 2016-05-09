3f40c12e3a0c9960beec8d749fb73bda5ac63266
SimonHGR <simon@dancingcloudservices.com>
2016-05-08 18:37:11 -0600

    Initial checkin, basic server with package.json for npm dependency management

---
+++ README.md
@@ -0,0 +1,9 @@
+Trivial Node/Express Server
+===========================
+
+This is a trivial http server built with node and express. It expects
+to serve JSON data, with all four main HTTP methods. 
+The data are stored in a simple JavaScript object structure, which
+facilitates easy creation of new keys along with some initialized
+data, and then deploying the server to support classroom lab exercises
+that require a minimal REST service to operate against.
---
+++ server.js
@@ -0,0 +1,91 @@
+"use strict";
+let express = require('express'),
+    app = express();
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/'));
+
+// initial data structure to serve:
+let data = {
+    customers: [
+        {name: 'Fred', limit: 1000},
+        {name: 'Jim', limit: 800}
+    ],
+   
+    animals: [
+        {species: 'Lion', carnivore: true},
+        {species: 'Horse', carnivore: false}
+    ]
+};
+
+app.get('/', function(req, res){
+    let rv = [];
+    for (let k in data) {
+        if (data.hasOwnProperty(k)) {
+            rv.push('/' + k);
+        }
+    }
+    res.status(200);
+    res.json(rv);
+});
+
+app.get('/:key', function(req, res){
+    let key = req.params.key;
+    let status = 404;
+    let rv = [];
+    if (data[key] !== undefined) {
+        status = 200;
+        rv = data[key];
+    }
+    res.status(status);
+    res.json(rv);
+});
+
+app.get('/:key/:id', function(req, res) {    
+    let id = parseInt(req.params.id);
+    let key = req.params.key;
+    console.log('GET /' + key + '/' + id);
+    res.json(data[key][id]);
+});
+
+app.post('/:key', function(req, res) {
+    let key = req.params.key;
+    console.log('POST /' + key + ' body is ' + JSON.stringify(req.body));
+    let id = data[key].length;
+    data[key][id] = req.body;
+    res.set('Location', '/' + key + '/' + id);
+    res.status(201);
+    res.send();
+});
+
+
+app.put('/:key/:id', function(req, res) {
+    let id = parseInt(req.params.id);
+    let key = req.params.key;
+    console.log('PUT /' + key + '/' + id + ' body is ' + JSON.stringify(req.body));
+    let status = 404;
+    if (id < data[key].length) {
+        data[key][id] = req.body;
+        status = 200;
+    }
+    res.status(status);
+    res.send();
+});
+
+app.delete('/:key/:id', function(req, res) {
+    let id = parseInt(req.params.id);
+    let key = req.params.key;
+    console.log('DELETE /' + key + '/' + id);
+    let status = 404;
+    if (id < data[key].length) {
+        data[key].splice(id, 1);
+        status = 204;
+    }
+    res.status(status);
+    res.send();
+});
+
+app.listen(8080);
+
+console.log('Express listening on port 8080');
